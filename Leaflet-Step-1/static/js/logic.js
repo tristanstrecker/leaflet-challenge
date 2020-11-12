d6f4ab6fb0779f39d69342daa53c8b5fa9041aec
Adrian <azmodea@gmail.com>
2020-11-12 12:56:14 -0600

    Backing up progress

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+// Create an initial map object
+// Set the longitude, latitude, and the starting zoom level
+let myMap = L.map("map").setView([39.8283, -98.5795], 4);
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
