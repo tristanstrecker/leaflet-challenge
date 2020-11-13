38ab5862775ac2c0cd9c5c408572ec39ffd10357
Adrian <azmodea@gmail.com>
2020-11-13 14:27:24 -0600

    Backing up changes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,9 @@
 // Create an initial map object
 // Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map").setView([39.8283, -98.5795], 4);
+var myMap = L.map("mapid").setView([39.8283, -98.5795], 4);
 
+
+// light-v10
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -16,7 +18,11 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+d3.json(queryUrl, function(eqData) {
+  // Creat GeoJSON layer with retrieved data
+  L.geoJson(eqData).addTo(myMap)
+  console.log(eqData)
+
+
+
 });
