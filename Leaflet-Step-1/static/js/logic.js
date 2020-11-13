1ace14d1aa341b6d563e608546d08e74d412a762
Adrian <azmodea@gmail.com>
2020-11-12 20:53:13 -0600

    Backing up changes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,10 +1,7 @@
-var myMap = L.map("map", {
-  center: [39.8283, -98.5795],
-  zoom: 4
-});
+// Create an initial map object
+// Set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map").setView([39.8283, -98.5795], 4);
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,6 +11,8 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+
+// Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
